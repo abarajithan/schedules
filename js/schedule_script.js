5afe7763aea2dc76d3e6be251ef34de3fd037ec0
Abarajithan <abar.nerve24@gmail.com>
2017-10-27 12:45:55 +0530

    Remove option and Add option fixes

--- js/schedule_script.js
+++ js/schedule_script.js
@@ -18,8 +18,8 @@
 						'<div>'+
 							'<div class="end-time" id="end-time-'+day+'-'+index+'"></div>'+
 						'</div>'+
-						'<div class="remove_img">'+
-							'<img day="'+day+'"id="remove-'+day+'-'+index+'" src="images/close.png">'+
+						'<div class="remove_img visibility-off">'+
+							'<img x-id="'+day+'-'+index+'" day="'+day+'"id="remove-'+day+'-'+index+'" src="images/close.png">'+
 						'</div>'+
 						'<div class="add_img">'+
 							'<img day="'+day+'"id="add_'+day+'_row" src="images/add_circle.png">'+
@@ -49,7 +49,7 @@
 })();
 
 
-$('.add_img img').click(function(){
+$('body').on('click', '.add_img img',function(){
 	var day = $(this).attr('day');
 	var index = $("#"+$(this).attr('day')+"-td").children().length;
 	var template = '<div class="'+day+'-'+index+'">'+
@@ -65,12 +65,43 @@ $('.add_img img').click(function(){
 						'<div>'+
 							'<div class="end-time" id="end-time-'+day+'-'+index+'"></div>'+
 						'</div>'+
-						'<div class="remove_img">'+
-							'<img day="'+day+'"id="remove-'+day+'-'+index+'" src="images/close.png">'+
+						'<div class="remove_img visibility-off">'+
+							'<img x-id="'+day+'-'+index+'" day="'+day+'"id="remove-'+day+'-'+index+'" src="images/close.png">'+
 						'</div>'+
 					'</div>';
-			$("#"+day+"-td").append(template);		
-			$("#"+day+"-start-datepicker-"+index ).datepicker();
-			$("#"+day+"-end-datepicker-"+index ).datepicker();
-			$("#"+day+"-start-timepicker-"+index ).timepicker();
+	$("#"+day+"-td").append(template);		
+	$("#"+day+"-start-datepicker-"+index ).datepicker();
+	$("#"+day+"-end-datepicker-"+index ).datepicker();
+	$("#"+day+"-start-timepicker-"+index ).timepicker();
+	if(index >= 1){
+		$("#"+day+"-td .remove_img").removeClass('visibility-off');
+		$("#"+day+"-td .remove_img").addClass('visibility-on');
+	}
+});
+
+$('body').on('click', '.remove_img img', function() {
+	var rowId = $(this).attr('x-id');
+	var day = rowId.split('-')[0];
+	var index = rowId.split('-')[1];
+    $('.'+rowId).remove();
+    var childrens = $("#"+day+"-td").children();
+    for (var i = parseInt(index); i < childrens.length; i++) {
+    	$(childrens[i]).attr('class',day+'-'+i);
+    	$(childrens[i]).find('#'+day+'-start-datepicker-'+(i+1)).attr('id',day+'-start-datepicker-'+i);
+    	$(childrens[i]).find('#'+day+'-end-datepicker-'+(i+1)).attr('id',day+'-end-datepicker-'+i);
+    	$(childrens[i]).find('#'+day+'-start-timepicker-'+(i+1)).attr('id',day+'-start-timepicker-'+i);
+    	$(childrens[i]).find('#end-time-'+day+'-'+(i+1)).attr('id','end-time-'+day+'-'+i);
+    	$(childrens[i]).find('#remove-'+day+'-'+(i+1)).attr('id','remove-'+day+'-'+i).attr('x-id',day+'-'+i);
+    	if(!i){
+    		var addImgTemplate = '<div class="add_img">'+
+									'<img day="'+day+'"id="add_'+day+'_row" src="images/add_circle.png">'+
+								'</div>';
+    		$(childrens[i]).find('.remove_img').after(addImgTemplate);
+    	}
+    }
+
+    if(childrens.length == 1){
+		$("#"+day+"-td .remove_img").removeClass('visibility-on');
+		$("#"+day+"-td .remove_img").addClass('visibility-off');
+	}
 });
